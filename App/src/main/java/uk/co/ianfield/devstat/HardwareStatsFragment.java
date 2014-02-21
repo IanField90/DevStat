006a20a685918b0288d93a995d42d4ccf4e35520
Ian Field <ian.field90@gmail.com>
2014-02-21 09:58:03 +0000

    Add more screen stats

--- App/src/main/java/uk/co/ianfield/devstat/HardwareStatsFragment.java
+++ App/src/main/java/uk/co/ianfield/devstat/HardwareStatsFragment.java
@@ -1,12 +1,32 @@
 package uk.co.ianfield.devstat;
 
 import android.support.v4.app.Fragment;
+import android.widget.ListView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
+
+import uk.co.ianfield.devstat.model.StatItem;
+import uk.co.ianfield.devstat.widget.StatItemArrayAdapter;
 
 /**
  * Created by ianfield on 20/02/2014.
  */
 @EFragment (R.layout.stats_list)
 public class HardwareStatsFragment extends Fragment {
+    @ViewById
+    ListView statsList;
+
+    ArrayList<StatItem> stats = new ArrayList<StatItem>();
+
+    @AfterViews
+    void initHardwareStats() {
+
+        StatItem stat;
+
+        statsList.setAdapter(new StatItemArrayAdapter(getActivity(), R.layout.stat_item, stats));
+    }
 }
--- App/src/main/java/uk/co/ianfield/devstat/ScreenStatsFragment.java
+++ App/src/main/java/uk/co/ianfield/devstat/ScreenStatsFragment.java
@@ -1,5 +1,6 @@
 package uk.co.ianfield.devstat;
 
+import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
 import android.widget.ListView;
@@ -19,7 +20,7 @@ import uk.co.ianfield.devstat.widget.StatItemArrayAdapter;
 @EFragment (R.layout.stats_list)
 public class ScreenStatsFragment extends Fragment {
     @ViewById
-    ListView screenStatsList;
+    ListView statsList;
 
     ArrayList<StatItem> stats = new ArrayList<StatItem>();
     DisplayMetrics metrics;
@@ -32,18 +33,59 @@ public class ScreenStatsFragment extends Fragment {
 
         stat = new StatItem();
         stat.setTitle(getString(R.string.screen_width));
-        stat.setInfo(String.format("%dpx", metrics.widthPixels));
+        stat.setInfo(String.format("%d px", metrics.widthPixels));
         stats.add(stat);
 
         stat = new StatItem();
         stat.setTitle(getString(R.string.screen_height));
-        stat.setInfo(String.format("%dpx", metrics.heightPixels));
+        stat.setInfo(String.format("%d px", metrics.heightPixels));
         stats.add(stat);
 
-        screenStatsList.setAdapter(new StatItemArrayAdapter(getActivity(), R.layout.stat_item, stats));
+        stat = new StatItem();
+        stat.setTitle(getString(R.string.display_density));
+        stat.setInfo(String.format("%d dpi", metrics.densityDpi));
+        stats.add(stat);
+
+        stat = new StatItem();
+        stat.setTitle(getString(R.string.drawable_density));
+        if (metrics.density == 0.75) {
+            stat.setInfo("ldpi");
+        }
+        else if (metrics.density == 1.0) {
+            stat.setInfo("mdpi");
+        }
+        else if (metrics.density == 1.5) {
+            stat.setInfo("hdpi");
+        }
+        else if (metrics.density == 2.0) {
+            stat.setInfo("xhdpi");
+        }
+        stats.add(stat);
+
+        stat = new StatItem();
+        stat.setTitle(getString(R.string.screen_size));
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        switch(screenSize) {
+            case Configuration.SCREENLAYOUT_SIZE_LARGE:
+                stat.setInfo(getString(R.string.screen_size_large));
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+                stat.setInfo(getString(R.string.screen_size_normal));
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_SMALL:
+                stat.setInfo(getString(R.string.screen_size_small));
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_XLARGE:
+                stat.setInfo(getString(R.string.screen_size_xlarge));
+                break;
+            default:
+                stat.setInfo(getString(R.string.screen_size_undefined));
+        }
+        stats.add(stat);
+
+        statsList.setAdapter(new StatItemArrayAdapter(getActivity(), R.layout.stat_item, stats));
     }
-
-
-
 
 }
