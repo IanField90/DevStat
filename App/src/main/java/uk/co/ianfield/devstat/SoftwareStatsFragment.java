e4f19027e7ca0da64d722f61f1368b07a659f2de
Ian Field <ian.field90@gmail.com>
2014-02-21 10:36:36 +0000

    Add some software stats

--- App/src/main/java/uk/co/ianfield/devstat/SoftwareStatsFragment.java
+++ App/src/main/java/uk/co/ianfield/devstat/SoftwareStatsFragment.java
@@ -1,5 +1,9 @@
 package uk.co.ianfield.devstat;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.widget.ListView;
 
@@ -25,6 +29,28 @@ public class SoftwareStatsFragment extends Fragment {
     @AfterViews
     void initSoftwareStats() {
         StatItem stat;
+
+        stat = new StatItem();
+        stat.setTitle(getString(R.string.android_version));
+        stat.setInfo(Build.VERSION.RELEASE);
+        stats.add(stat);
+
+        stat = new StatItem();
+        stat.setTitle(getString(R.string.sdk_int));
+        stat.setInfo(String.format("%d", Build.VERSION.SDK_INT));
+        stats.add(stat);
+
+        stat = new StatItem();
+        stat.setTitle(getString(R.string.opengl_version));
+        ActivityManager activityManager = (ActivityManager) getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        if (configurationInfo != null) {
+            stat.setInfo(configurationInfo.getGlEsVersion());
+        }
+        else {
+            stat.setInfo(getString(R.string.unknown));
+        }
+        stats.add(stat);
 
         statsList.setAdapter(new StatItemArrayAdapter(getActivity(), R.layout.stat_item, stats));
     }
