a5bb373ffadd564e0f76a59d57130b147cc9400c
Ian Field <ian.field90@gmail.com>
2014-11-15 16:49:44 +0000

    Update to be single view only

--- App/src/main/java/uk/co/ianfield/devstat/AboutActivity.java
+++ App/src/main/java/uk/co/ianfield/devstat/AboutActivity.java
@@ -1,12 +1,23 @@
 package uk.co.ianfield.devstat;
 
 import android.support.v7.app.ActionBarActivity;
+import android.webkit.WebView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by ianfield on 21/02/2014.
  */
 @EActivity(R.layout.activity_about)
 public class AboutActivity extends ActionBarActivity {
+
+    @ViewById(R.id.webView)
+    WebView webView;
+
+    @AfterViews
+    void initAbout() {
+        webView.loadUrl("file:///android_asset/about.html");
+    }
 }
