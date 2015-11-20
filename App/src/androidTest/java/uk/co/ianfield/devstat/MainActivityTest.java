8c465845be86cd7d1239caa1895118de1aa1f58b
Ian Field <ian.field90@gmail.com>
2015-11-20 23:00:21 +0000

    Add testing capability

---
+++ App/src/androidTest/java/uk/co/ianfield/devstat/MainActivityTest.java
@@ -0,0 +1,37 @@
+package uk.co.ianfield.devstat;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Ian on 20/11/2015.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity_> mActivityRule = new ActivityTestRule(MainActivity_.class);
+
+    @Test
+    public void checkAboutIsLaunched() {
+        Intents.init();
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText(R.string.action_about)).perform(click());
+        intended(hasComponent(AboutActivity_.class.getName()));
+        Intents.release();
+    }
+
+}
