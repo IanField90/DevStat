684eb21d532bd2cddc882ae3277a8cb89db66a7f
= <ian.field90@gmail.com>
2017-07-27 22:09:03 +0100

    Attempt to fix build

--- app/src/test/java/uk/co/ianfield/devstat/model/StatItemTest.kt
+++ app/src/test/java/uk/co/ianfield/devstat/model/StatItemTest.kt
@@ -1,26 +1,39 @@
 package uk.co.ianfield.devstat.model
 
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations.initMocks
+import org.mockito.Mockito.`when` as whenMock
 
 /**
  * Created by Ian on 20/11/2015.
  */
 @RunWith(JUnit4::class)
 class StatItemTest {
+    @Mock lateinit var statItem: StatItem
+
+    @Before
+    fun setUp() {
+        initMocks(this)
+    }
 
     @Test
+    @Throws(Exception::class)
     fun testToString() {
 //        var item = mock<StatItem>(StatItem::class.java)
-//        `when`(item.title).thenReturn("a")
-//        `when`(item.info).thenReturn("b")
-//        assertThat(item.title).isEqualTo("a")
-//
-//        // Can't mock toString()
-//        item = StatItem()
-//        item.title = "a"
-//        item.info = "b"
-//        assertThat(item.toString()).isEqualTo("a: b")
+        whenMock(statItem.title).thenReturn("a")
+        whenMock(statItem.info).thenReturn("b")
+        assertThat(statItem.title).isEqualTo("a")
+
+        // Can't mock toString()
+        statItem = StatItem()
+        statItem.title = "a"
+        statItem.info = "b"
+        assertThat(statItem.toString()).isEqualTo("a: b")
     }
 }
