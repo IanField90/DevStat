da906a77d001ce982a18f7b8c1d776d5db3b7204
Ian Field <ian.field90@gmail.com>
2021-09-21 21:55:21 +0100

    Migrate to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.5.21'
-    ext.hilt_version = '2.38.1'
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.0.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath "com.google.dagger:hilt-android-gradle-plugin:$hilt_version"
+        classpath(libs.android.pluginGradle)
+        classpath(libs.kotlin.pluginGradle)
+        classpath(libs.hilt.pluginGradle)
     }
 }
 
